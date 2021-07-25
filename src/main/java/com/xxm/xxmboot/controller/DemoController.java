da95b18599503852f100db009d3c475206feef84
ericxuxxm <eircxu@163.com>
2021-07-25 22:54:36 +0800

    hello

--- src/main/java/com/xxm/xxmboot/controller/DemoController.java
+++ src/main/java/com/xxm/xxmboot/controller/DemoController.java
@@ -1,4 +1,13 @@
 package com.xxm.xxmboot.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class DemoController {
+
+    @GetMapping
+    public String hello(String name){
+        return "hello"+name;
+    }
 }
