c50f2e7c05a0649dfda57b682cf4e909510e2b52
安安的狒狒 <1505552568@qq.com>
2021-07-25 21:45:57 +0800

    新建boot项目

---
+++ src/test/java/com/xxm/xxmboot/XxmBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xxm.xxmboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XxmBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
